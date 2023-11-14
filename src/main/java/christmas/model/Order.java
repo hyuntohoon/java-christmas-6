2e90656d793ea7a4af1e0769242a9b90dc94178f
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-14 17:23:45 +0900

    refactor : orderDate 필드 제거 => EventPlanner로 이동

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -4,11 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Order {
-    private final int orderDate;
     private final Map<Menu, Integer> orderItems;
 
-    public Order(int orderDate, String orderMenuString) {
-        this.orderDate = orderDate;
+    public Order(String orderMenuString) {
         this.orderItems = new HashMap<>();
         parseAndAddItems(orderMenuString);
     }
