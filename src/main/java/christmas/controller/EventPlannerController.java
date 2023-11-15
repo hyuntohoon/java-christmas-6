1c9c4a7215e22f0136548d9ac5d225cb57bc0561
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 17:54:59 +0900

    feat : EventPlannerController 클래스 생성

---
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -0,0 +1,23 @@
+package christmas.controller;
+
+import christmas.model.EventPlanner;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class EventPlannerController {
+
+    public void run() {
+        int orderDay = InputView.readOrderDate();
+        String orderMenuString = InputView.readOrderMenu();
+
+        EventPlanner eventPlanner = new EventPlanner(orderDay, orderMenuString);
+
+        OutputView.displayWelcomeMessage();
+        OutputView.displayOrderDetails(eventPlanner.getOrderDetails());
+        OutputView.displayTotalBeforeDiscount(eventPlanner.calculateTotalBeforeDiscount());
+        OutputView.displayTotalDiscount(eventPlanner.calculateTotalDiscount());
+        OutputView.displayTotalAfterDiscount(eventPlanner.calculateTotalAfterDiscount());
+        OutputView.displayBadgeDetails(eventPlanner.getEventBadgeDetails());
+        OutputView.displayGiftItemDetails(eventPlanner.getGiftItemDetails());
+    }
+}
