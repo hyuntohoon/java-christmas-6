40c741ea25973276151319313f8f4da7d60602b8
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 20:13:08 +0900

    fix : 주말 날짜 수정

--- src/main/java/christmas/model/Calendar.java
+++ src/main/java/christmas/model/Calendar.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public enum Calendar {
     ;
     private static final Set<Integer> weekendDays = Set.of(
-            3, 10, 17, 24, 25, 31
+            2, 3, 9, 10, 16, 17, 23, 24, 30, 31
     );
 
     private static final Set<Integer> specialEventDays = Set.of(
