84ccb1c91d46be1f7d319bb3fe60ed64657ceee0
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 21:28:37 +0900

    feat : 10000원 이하 할인 미적용 기능 추가

--- src/main/java/christmas/model/DiscountManager.java
+++ src/main/java/christmas/model/DiscountManager.java
@@ -6,27 +6,33 @@ import java.util.Map;
 public class DiscountManager {
     private Map<String, Integer> discounts;
     private int giftEventDiscount;
+    private static final int MINIMUM_AMOUNT_FOR_DISCOUNT = 10000;
+
     public DiscountManager() {
         this.discounts = new HashMap<>();
         this.giftEventDiscount = 0;
     }
 
-
     public void calculateDiscounts(Order order, int orderDay) {
+        int totalOrderAmount = order.calculateTotalPrice(order);
+        if (totalOrderAmount <= MINIMUM_AMOUNT_FOR_DISCOUNT) {
+            return; // 주문 금액이 최소 요구 금액보다 적으면 할인 없음
+        }
+
         Map<Menu, Integer> orderItems = order.getOrderItems();
         boolean isWeekend = Calendar.isWeekend(orderDay);
         boolean isWeekday = !isWeekend;
         boolean isSpecialEvent = Calendar.isSpecialEventDay(orderDay);
-        this.giftEventDiscount = calculateGiftEventDiscount(order);
+
+        // 각 할인 항목 계산
         addDiscount("크리스마스 디데이 할인", Discount.calculateChristmasDdayDiscount(orderDay));
         addDiscount("평일 디저트 할인", Discount.calculateWeekdayDessertDiscount(orderItems, isWeekday));
         addDiscount("주말 메인 할인", Discount.calculateWeekendMainDiscount(orderItems, isWeekend));
         addDiscount("특별 이벤트 할인", Discount.calculateSpecialEventDiscount(isSpecialEvent));
 
-        int giftDiscount = calculateGiftEventDiscount(order);
-        this.giftEventDiscount = giftDiscount;
-        if (giftDiscount > 0) {
-            discounts.put("증정 이벤트", giftDiscount);
+        this.giftEventDiscount = calculateGiftEventDiscount(order);
+        if (giftEventDiscount > 0) {
+            discounts.put("증정 이벤트", giftEventDiscount);
         }
     }
 
