425d1083eb056fc5354cf07a0558193fcd1c7ae4
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 09:24:10 +0900

    feat : calculateChristmasDdayDiscount 함수 추가

--- src/main/java/christmas/model/Discount.java
+++ src/main/java/christmas/model/Discount.java
@@ -5,4 +5,11 @@ public class Discount {
     private static final int WEEKEND_MAIN_DISCOUNT = 2023;
     private static final int SPECIAL_EVENT_DISCOUNT = 1000;
 
+    public static int calculateChristmasDdayDiscount(int orderDay) {
+        if (orderDay >= 1 && orderDay <= 25) {
+            return 1000 + (orderDay - 1) * 100;
+        }
+        return 0;
+    }
+
 }
