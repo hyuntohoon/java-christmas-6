30a6fe662ecb12fb704cb546cc4929272ff6ca83
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 12:33:41 +0900

    feat : GiftEvent 변수명 및 getter 추가

--- src/main/java/christmas/model/GiftEvent.java
+++ src/main/java/christmas/model/GiftEvent.java
@@ -4,19 +4,27 @@ public enum GiftEvent {
     CHAMPAGNE_GIFT(120000, "샴페인"); // 예: 12만원 이상 구매 시 샴페인 증정
 
     private final int threshold;
-    private final String giftItem;
+    private final String giftName;
 
-    GiftEvent(int threshold, String giftItem) {
+    GiftEvent(int threshold, String giftName) {
         this.threshold = threshold;
-        this.giftItem = giftItem;
+        this.giftName = giftName;
     }
 
-    public static String determineGift(int totalAmount) {
+    public static GiftEvent determineGift(int totalAmount) {
         for (GiftEvent event : GiftEvent.values()) {
             if (totalAmount >= event.threshold) {
-                return event.giftItem;
+                return event;
             }
         }
-        return "";
+        return null; // 증정 아이템이 없는 경우
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public String getGiftName() {
+        return giftName;
     }
 }
