c3bd418baf92f6cc2b2a22e162beca322b1090af
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 19:07:20 +0900

    refactor : 출력 형식 변경

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,6 +2,7 @@ package christmas.view;
 
 public class OutputView {
 
+
     public static void displayWelcomeMessage() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
@@ -13,13 +14,18 @@ public class OutputView {
 
     public static void displayTotalBeforeDiscount(int totalBeforeDiscount) {
         System.out.println("\n<할인 전 총주문 금액>");
-        System.out.println(totalBeforeDiscount + "원");
+        System.out.println(String.format("%,d원", totalBeforeDiscount));
     }
 
     public static void displayGiftItemDetails(String giftItemDetails) {
         System.out.println("\n<증정 메뉴>");
-        System.out.println(giftItemDetails.equals("") ? "없음" : giftItemDetails);
+        if (giftItemDetails.isEmpty()) {
+            System.out.println("없음");
+            return;
+        }
+        System.out.println(giftItemDetails);
     }
+
 
     public static void displayDiscountDetails(String discountDetails) {
         System.out.println("\n<혜택 내역>");
@@ -29,7 +35,7 @@ public class OutputView {
     public static void displayTotalDiscount(int totalDiscount) {
         System.out.println("\n<총혜택 금액>");
         if (totalDiscount > 0) {
-            System.out.println("-" + totalDiscount + "원");
+            System.out.println("-" + String.format("%,d원", totalDiscount));
             return;
         }
         System.out.println("없음");
@@ -37,7 +43,7 @@ public class OutputView {
 
     public static void displayTotalAfterDiscount(int totalAfterDiscount) {
         System.out.println("\n<할인 후 예상 결제 금액>");
-        System.out.println(totalAfterDiscount + "원");
+        System.out.println(String.format("%,d원", totalAfterDiscount));
     }
 
     public static void displayBadgeDetails(String badgeDetails) {
