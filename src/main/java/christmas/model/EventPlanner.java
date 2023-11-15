e2a44247760c6525de2beaca6e16ee7ffdfdba22
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 13:10:57 +0900

    refactor : 필드명 줄이고, calculateTotalPrice 실행 위치 변경

--- src/main/java/christmas/model/EventPlanner.java
+++ src/main/java/christmas/model/EventPlanner.java
@@ -3,41 +3,31 @@ package christmas.model;
 import java.util.Map;
 
 public class EventPlanner {
-    private Order order;
-    private int orderDay;
-    private int totalBeforeDiscount;
-    private int totalDiscount;
-    private int totalAfterDiscount;
-    private Badge eventBadge;
-    private GiftEvent giftEvent;
-
-    private DiscountManager discountManager;
+    private final Order order;
+    private final int orderDay;
+    private final Badge eventBadge;
+    private final GiftEvent giftEvent;
+    private final DiscountManager discountManager;
 
     public EventPlanner(int orderDay, String orderMenuString) {
         this.orderDay = orderDay;
         this.order = new Order(orderMenuString);
         this.discountManager = new DiscountManager();
-        calculateOrderDetails();
-    }
-
-    private void calculateOrderDetails() {
-        this.totalBeforeDiscount = order.calculateTotalPrice();
         this.discountManager.calculateDiscounts(order, orderDay);
-        this.totalDiscount = this.discountManager.getTotalDiscount();
-        this.totalAfterDiscount = this.totalBeforeDiscount - this.totalDiscount;
-        this.eventBadge = Badge.getBadgeForAmount(totalDiscount);
-        this.giftEvent = GiftEvent.determineGift(totalBeforeDiscount);
+        this.eventBadge = Badge.getBadgeForAmount(discountManager.getTotalDiscount());
+        this.giftEvent = GiftEvent.determineGift(discountManager.calculateTotalPrice(order));
     }
 
-    private int calculateTotalDiscount() {
-        boolean isWeekend = Calendar.isWeekend(this.orderDay);
-        boolean isSpecialEvent = Calendar.isSpecialEventDay(this.orderDay);
-        int discount = 0;
-        Map<Menu, Integer> orderItems = this.order.getOrderItems();
-        discount += Discount.calculateWeekendMainDiscount(orderItems, isWeekend);
-        discount += Discount.calculateSpecialEventDiscount(isSpecialEvent);
-        discount += Discount.calculateChristmasDdayDiscount(this.orderDay);
-        return discount;
+    public int getTotalBeforeDiscount() {
+        return discountManager.calculateTotalPrice(order);
+    }
+
+    public int getTotalDiscount() {
+        return discountManager.getTotalDiscount();
+    }
+
+    public int getTotalAfterDiscount() {
+        return getTotalBeforeDiscount() - getTotalDiscount();
     }
 
 }
