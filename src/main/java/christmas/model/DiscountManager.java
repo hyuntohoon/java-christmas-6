36e4175ed138947354fe31c021ae3ccf74e473b4
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 12:56:12 +0900

    feat : calculateDiscounts 함수 추가

--- src/main/java/christmas/model/DiscountManager.java
+++ src/main/java/christmas/model/DiscountManager.java
@@ -9,4 +9,16 @@ public class DiscountManager {
     public DiscountManager() {
         this.discounts = new HashMap<>();
     }
+
+    public void calculateDiscounts(Order order, int orderDay) {
+        Map<Menu, Integer> orderItems = order.getOrderItems();
+        boolean isWeekend = Calendar.isWeekend(orderDay);
+        boolean isWeekday = !isWeekend;
+        boolean isSpecialEvent = Calendar.isSpecialEventDay(orderDay);
+
+        addDiscount("크리스마스 디데이 할인", Discount.calculateChristmasDdayDiscount(orderDay));
+        addDiscount("평일 디저트 할인", Discount.calculateWeekdayDessertDiscount(orderItems, isWeekday));
+        addDiscount("주말 메인 할인", Discount.calculateWeekendMainDiscount(orderItems, isWeekend));
+        addDiscount("특별 이벤트 할인", Discount.calculateSpecialEventDiscount(isSpecialEvent));
+    }
 }
