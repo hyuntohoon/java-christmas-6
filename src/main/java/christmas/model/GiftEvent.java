ec4a23c339ffb5da315dd2a6f0467a6d379fa831
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 14:25:15 +0900

    refactor : GiftEvnet 확장성 고려하여 필드 재정의

--- src/main/java/christmas/model/GiftEvent.java
+++ src/main/java/christmas/model/GiftEvent.java
@@ -1,23 +1,28 @@
 package christmas.model;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public enum GiftEvent {
-    CHAMPAGNE_GIFT(120000, "샴페인"); // 예: 12만원 이상 구매 시 샴페인 증정
+    CHAMPAGNE_GIFT(120000, "샴페인", 1, 25000); // 예: 12만원 이상 구매 시 샴페인 1개 증정
 
     private final int threshold;
     private final String giftName;
+    private final int quantity;
+    private final int giftPrice;
 
-    GiftEvent(int threshold, String giftName) {
+    GiftEvent(int threshold, String giftName, int quantity, int giftPrice) {
         this.threshold = threshold;
         this.giftName = giftName;
+        this.quantity = quantity;
+        this.giftPrice = giftPrice;
     }
 
-    public static GiftEvent determineGift(int totalAmount) {
-        for (GiftEvent event : GiftEvent.values()) {
-            if (totalAmount >= event.threshold) {
-                return event;
-            }
-        }
-        return null; // 증정 아이템이 없는 경우
+    public static Map<String, Integer> determineGifts(int totalAmount) {
+        return java.util.Arrays.stream(GiftEvent.values())
+                .filter(event -> totalAmount >= event.threshold)
+                .collect(Collectors.toMap(event -> event.giftName, event -> event.quantity, (e1, e2) -> e1, LinkedHashMap::new));
     }
 
     public int getThreshold() {
@@ -26,5 +31,13 @@ public enum GiftEvent {
 
     public String getGiftName() {
         return giftName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getGiftPrice() {
+        return giftPrice;
     }
 }
