7021ffbf6ae3c55ca5619d619d7c22db8dba0dfc
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 09:22:03 +0900

    feat : 상수 선언

---
+++ src/main/java/christmas/model/Discount.java
@@ -0,0 +1,8 @@
+package christmas.model;
+
+public class Discount {
+    private static final int WEEKDAY_DESSERT_DISCOUNT = 2023;
+    private static final int WEEKEND_MAIN_DISCOUNT = 2023;
+    private static final int SPECIAL_EVENT_DISCOUNT = 1000;
+
+}
