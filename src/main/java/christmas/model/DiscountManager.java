f3b0a34f4bc49afef0b9dd6b73bf961e5ba78b90
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2023-11-15 12:54:08 +0900

    feat : DiscountManager 필드 및 생성자 선언

---
+++ src/main/java/christmas/model/DiscountManager.java
@@ -0,0 +1,12 @@
+package christmas.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DiscountManager {
+    private Map<String, Integer> discounts;
+
+    public DiscountManager() {
+        this.discounts = new HashMap<>();
+    }
+}
